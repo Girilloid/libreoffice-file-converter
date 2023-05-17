42a20a38205cfcf300221aea73deb2e657c4163b
Ilya Marozau <girilliamor@outlook.com>
2023-05-17 10:19:00 +0200

    fix: Return dts, add commonjs/esm exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,14 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   clean: true,
+  dts: true,
   entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  outDir: 'dist',
   platform: 'node',
   skipNodeModulesBundle: true,
   sourcemap: true,
-  splitting: false,
+  splitting: true,
+  target: 'es2020',
   treeshake: true,
 });
