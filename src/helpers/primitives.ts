22180275d7d256d6513b5d6c997cfe4b47b4e0f0
Ilya Marozau <girilliamor@outlook.com>
2024-08-20 11:15:49 +0200

    fix: Get rid of structureClone for better compatibility with node < 17

--- src/helpers/primitives.ts
+++ src/helpers/primitives.ts
@@ -1,27 +1,28 @@
-export const isObject = (item: unknown): boolean => {
+export const isObject = (item: unknown): item is Record<string, unknown> => {
   return Boolean(item && typeof item === 'object' && !Array.isArray(item));
 };
 
-export const deepMerge = <T extends object>(target: T, ...sources: readonly T[]): T => {
-  if (!sources.length) {
-    return target;
-  }
+export const deepMerge = <T extends Record<string, unknown>>(...objects: readonly T[]): T => {
+  return objects.reduce<T>((target, source) => {
+    Object.keys(source).forEach((key: keyof T) => {
+      const targetValue = target[key];
+      const sourceValue = source[key];
 
-  const [source, ...restSources] = sources;
+      if (Array.isArray(targetValue) && Array.isArray(sourceValue)) {
+        target[key] = [...targetValue, ...sourceValue] as T[keyof T];
 
-  if (isObject(target) && isObject(source)) {
-    for (const key in source) {
-      if (isObject(source[key])) {
-        if (!target[key]) {
-          Object.assign(target, { [key]: {} });
-        }
-
-        deepMerge(target[key] as object, source[key] as object);
-      } else {
-        Object.assign(target, { [key]: source[key] });
+        return;
       }
-    }
-  }
 
-  return deepMerge(target, ...restSources);
+      if (isObject(targetValue) && isObject(sourceValue)) {
+        target[key] = deepMerge(targetValue, sourceValue);
+
+        return;
+      }
+
+      target[key] = sourceValue;
+    });
+
+    return target;
+  }, {} as T);
 };
