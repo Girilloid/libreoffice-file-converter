e6df74a47da455abfc628e8e1ac0c569d7018342
Ilya Marozau <girilliamor@outlook.com>
2024-08-30 20:36:09 +0200

    test: Fix fs helpers tests to pass on windows

--- tests/fs.spec.ts
+++ tests/fs.spec.ts
@@ -1,40 +1,42 @@
+import { sep } from 'node:path';
+
 import { getProcessedFilePath } from '../src/helpers/fs';
 
 describe('helpers/fs', () => {
   describe('getProcessedFilePath', () => {
     const format = 'pdf';
-    const temporaryDirPath = '/tmp/libreoffice-file-converter';
+    const temporaryDirPath = ['tmp', 'libreoffice-file-converter'].join(sep);
 
     it('returns correct file path for files inside temporary dir', () => {
-      const inputPath = `${temporaryDirPath}/source`;
+      const inputPath = [temporaryDirPath, 'source'].join(sep);
 
       const result = getProcessedFilePath(temporaryDirPath, inputPath, format);
 
-      expect(result).toBe('/tmp/libreoffice-file-converter/source.pdf');
+      expect(result).toBe(['tmp', 'libreoffice-file-converter', 'source.pdf'].join(sep));
     });
 
     it('returns correct file path for files outside temporary dir', () => {
-      const inputPath = '/home/user/source.doc';
+      const inputPath = ['home', 'user', 'source.doc'].join(sep);
 
       const result = getProcessedFilePath(temporaryDirPath, inputPath, format);
 
-      expect(result).toBe('/tmp/libreoffice-file-converter/source.pdf');
+      expect(result).toBe(['tmp', 'libreoffice-file-converter', 'source.pdf'].join(sep));
     });
 
     it('returns correct file path for files outside temporary dir with extra dots in file name', () => {
-      const inputPath = '/home/user/source.to.process.doc';
+      const inputPath = ['home', 'user', 'source.to.process.doc'].join(sep);
 
       const result = getProcessedFilePath(temporaryDirPath, inputPath, format);
 
-      expect(result).toBe('/tmp/libreoffice-file-converter/source.to.process.pdf');
+      expect(result).toBe(['tmp', 'libreoffice-file-converter', 'source.to.process.pdf'].join(sep));
     });
 
     it('returns correct file path for files outside temporary dir without extension', () => {
-      const inputPath = '/home/user/source';
+      const inputPath = ['home', 'user', 'source'].join(sep);
 
       const result = getProcessedFilePath(temporaryDirPath, inputPath, format);
 
-      expect(result).toBe('/tmp/libreoffice-file-converter/source.pdf');
+      expect(result).toBe(['tmp', 'libreoffice-file-converter', 'source.pdf'].join(sep));
     });
   });
 });
--- tests/fs.test.ts
+++ tests/fs.test.ts
@@ -1,3 +1,5 @@
+import { sep } from 'node:path';
+
 import { assertEquals } from '@std/assert';
 import { describe, it } from '@std/testing/bdd';
 
@@ -6,38 +8,38 @@ import { getProcessedFilePath } from '../src/helpers/fs';
 describe('helpers/fs', () => {
   describe('getProcessedFilePath', () => {
     const format = 'pdf';
-    const temporaryDirPath = '/tmp/libreoffice-file-converter';
+    const temporaryDirPath = ['tmp', 'libreoffice-file-converter'].join(sep);
 
     it('returns correct file path for files inside temporary dir', () => {
-      const inputPath = `${temporaryDirPath}/source`;
+      const inputPath = [temporaryDirPath, 'source'].join(sep);
 
       const result = getProcessedFilePath(temporaryDirPath, inputPath, format);
 
-      assertEquals(result, '/tmp/libreoffice-file-converter/source.pdf');
+      assertEquals(result, ['tmp', 'libreoffice-file-converter', 'source.pdf'].join(sep));
     });
 
     it('returns correct file path for files outside temporary dir', () => {
-      const inputPath = '/home/user/source.doc';
+      const inputPath = ['home', 'user', 'source.doc'].join(sep);
 
       const result = getProcessedFilePath(temporaryDirPath, inputPath, format);
 
-      assertEquals(result, '/tmp/libreoffice-file-converter/source.pdf');
+      assertEquals(result, ['tmp', 'libreoffice-file-converter', 'source.pdf'].join(sep));
     });
 
     it('returns correct file path for files outside temporary dir with extra dots in file name', () => {
-      const inputPath = '/home/user/source.to.process.doc';
+      const inputPath = ['home', 'user', 'source.to.process.doc'].join(sep);
 
       const result = getProcessedFilePath(temporaryDirPath, inputPath, format);
 
-      assertEquals(result, '/tmp/libreoffice-file-converter/source.to.process.pdf');
+      assertEquals(result, ['tmp', 'libreoffice-file-converter', 'source.to.process.pdf'].join(sep));
     });
 
     it('returns correct file path for files outside temporary dir without extension', () => {
-      const inputPath = '/home/user/source';
+      const inputPath = ['home', 'user', 'source'].join(sep);
 
       const result = getProcessedFilePath(temporaryDirPath, inputPath, format);
 
-      assertEquals(result, '/tmp/libreoffice-file-converter/source.pdf');
+      assertEquals(result, ['tmp', 'libreoffice-file-converter', 'source.pdf'].join(sep));
     });
   });
 });
